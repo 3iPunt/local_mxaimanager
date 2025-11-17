b68abe384216043a687554748242aa3a09272bd6
Frederik Milling Pytlick <fmp@praxis.dk>
2025-11-17 11:24:53 +0100

    Make work on Moodle 4.4

--- classes/output/manage_providers/form_provider_supports.php
+++ classes/output/manage_providers/form_provider_supports.php
@@ -9,8 +9,8 @@ defined('MOODLE_INTERNAL') || die();
 // @codeCoverageIgnoreEnd
 
 use core\output\named_templatable;
-use core\output\renderable;
-use core\output\renderer_base;
+use renderable;
+use renderer_base;
 
 class form_provider_supports implements named_templatable, renderable
 {
--- version.php
+++ version.php
@@ -6,7 +6,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
 $plugin->component = 'local_mxaimanager';
-$plugin->version = 2025100100;
+$plugin->version = 2025111700;
 $plugin->requires = 2024042200; // Requires Moodle 4.4
 $plugin->release = '1.0.0 (Build: 2025-10-01)';
 $plugin->dependencies = [];
