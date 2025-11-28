6b6255c1e2e833c1231b1879f312d367a0c7de7a
Frederik Milling Pytlick <fmp@praxis.dk>
2025-11-28 15:22:33 +0100

    #6 - Started working on preconfigured providers

--- classes/output/manage_providers/table.php
+++ classes/output/manage_providers/table.php
@@ -13,10 +13,12 @@ use local_mxaimanager\app\factory as base_factory;
 class table extends \local_mxaimanager\app\table
 {
     private base_factory $base_factory;
+    private \moodle_url $url;
 
     public function __construct(base_factory $base_factory, \moodle_url $url)
     {
         $this->base_factory = $base_factory;
+        $this->url = $url;
 
         $headers = [];
         $columns = [];
@@ -40,9 +42,22 @@ class table extends \local_mxaimanager\app\table
         $this->collapsible(false);
         $this->sortable(true);
 
-        $this->set_sql("*", "{local_mxaimanager_providers}", "1=1");
+        $this->set_sortdata([]);
 
         parent::__construct($url, "manage_providers_table");
+    }
+
+    public function query_db($pagesize, $useinitialsbar = true)
+    {
+        $providers = $this->base_factory->ai()->provider()->repository()->get_all();
+        foreach ($providers as $provider) {
+            $this->add_data_keyed([
+                'name' => $provider->get_name(),
+                'classname' => $this->col_classname((object)$provider->to_array()),
+                'supported_actions' => $this->col_supported_actions((object)$provider->to_array()),
+                'actions' => $this->col_actions((object)$provider->to_array()),
+            ]);
+        }
     }
 
     protected function col_classname(object $record): string
